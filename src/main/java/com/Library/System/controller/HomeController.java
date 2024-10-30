6164166889a37645937ea8dd858f9a1f52c252fd
Ojay95 <112874084+Ojay95@users.noreply.github.com>
2024-10-30 17:01:30 +0100

    Fouth Commit

--- src/main/java/com/Library/System/controller/HomeController.java
+++ src/main/java/com/Library/System/controller/HomeController.java
@@ -2,12 +2,14 @@ package com.Library.System.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HomeController {
 
     @GetMapping("/")
     @ResponseBody
     public String home() {
         return "Welcome to Our Library System!";
-}
+    }
 }
